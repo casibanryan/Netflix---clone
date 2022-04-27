bda33e65e62981c8cacee046524c7994bcf60ae9
casibanryan <restyochea@gmail.com>
2022-04-27 22:32:49 +0800

    making the page responsive

--- src/include/Footer.js
+++ src/include/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <>
         {/* Footer*/}
-    <footer className="footer bg-black pt-sm-5">
+    <footer className="footer bg-black pt-sm-14 ">
         <div className="container">
             <div className="row">
                 <div className="col-lg-6 h-100 text-center text-lg-start my-auto">
