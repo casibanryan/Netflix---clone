12fe9e22f380ed4ffddee9d6ef6c697247b31e51
casibanryan <restyochea@gmail.com>
2022-04-27 16:33:20 +0800

    updating

--- src/include/Footer.js
+++ src/include/Footer.js
@@ -38,6 +38,17 @@ function Footer() {
     </footer>
     
     <Helmet>
+        {/* Bootstrap icons*/}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"
+        type="text/css" />
+
+    {/* font awesome*/}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+        {/* Google fonts*/}
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet"
+        type="text/css" />
            {/* Bootstrap core JS*/}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </Helmet>
