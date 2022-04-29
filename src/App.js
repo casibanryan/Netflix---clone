854e48547d5181cec0c6377fc03da393304a6acb
casibanryan <restyochea@gmail.com>
2022-04-29 23:00:21 +0800

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react'
 // import {Routes, Route} from 'react-router-dom';
 
 import Row from './Row';
+import requests from './js/requests';
 
 // Api key =>  14d57525a7a6772b9699d35339fb5878
 
@@ -17,7 +18,10 @@ function App(props) {
   //       </React.Fragment>
   // 
 
-    <Row title="Netflix originals" />
+    <>
+      <Row title="Netflix originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} /> 
+    </>
   )
 }
 
--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axiosInstance from './js/axios';
+
 
 function Row({ title }) {
+
+    const [movies, setMovies] = useState([]);
+
+    // making request to TMDB
+    // every time the row reloads
+    useEffect(() => {
+
+        // using async function to make the program fast
+        async function fetchData() {
+            const request = await axiosInstance.get(fetchUrl);
+            console.log(request);
+            return request;
+        }            
+        
+        fetchData();
+ 
+     }, [])    // if [] => dependency is empty means run once when page loads and dont't run again
+    
   return (
     <React.Fragment>
         {/* title */}
