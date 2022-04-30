09b7501a6d99bb2834bd5d01bff6d8e931d98d05
casibanryan <restyochea@gmail.com>
2022-04-30 13:24:45 +0800

    updating

--- src/pages/sections/home/HeroArea.js
+++ src/pages/sections/home/HeroArea.js
@@ -3,8 +3,7 @@ import slide2 from '../../../assets/img/slide2.png'
 import slide3 from '../../../assets/img/slide3.png'
 import slide1 from '../../../assets/img/slide1.png'
 import cast1 from '../../../assets/img/cast/cast1.png';
-import cast2 from '../../../assets/img/cast/cast3.png';
-import cast3 from '../../../assets/img/cast/cast3.png';
+
 
 
 function HeroArea() {
@@ -15,11 +14,11 @@ function HeroArea() {
 			<div className="container">
 				<div className="hero-area-slider">
 
-					{/* map start here in hero images */}
+				
 					<div className="row hero-area-slide">
 						<div className="col-lg-6 col-md-5">
 							<div className="hero-area-content">
-								<img src={slide2} alt="slide2" />
+								<img src={slide2} alt="about" />
 							</div>
 						</div>
 						<div className="col-lg-6 col-md-7">
@@ -41,24 +40,20 @@ function HeroArea() {
 									<div className="single-slide-cast">
 										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast2} alt="about" />
+                                    	<div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
+                                    	<div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
+                                    	<div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
+
+                                    	<div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
+								
 									<div className="single-slide-cast text-center">
 										5+
 									</div>
@@ -73,7 +68,6 @@ function HeroArea() {
 						</div>
 					</div>
 
-					{/* remove this line*/}
 					<div className="row hero-area-slide">
 						<div className="col-lg-6 col-md-5">
 							<div className="hero-area-content">
@@ -96,27 +90,25 @@ function HeroArea() {
 								<p>She is a devil princess from the demon world. She grew up sheltered by her parents and doesn't really know how to be evil or any of the common actions,   She is unable to cry due to Keita's accidental first wish, despite needed for him to wish...</p>
 								<h3>Cast:</h3>
 								<div className="slide-cast">
+									<div className="single-slide-cast">
+										<img src={cast1} alt="about" />
+									</div>
                                     <div className="single-slide-cast">
 										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast2} alt="about" />
+                                    <div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
+                                    <div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
+                                    <div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
+                                    <div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
+									
 									<div className="single-slide-cast text-center">
 										5+
 									</div>
@@ -129,7 +121,6 @@ function HeroArea() {
 						</div>
 					</div>
 
-					{/* remove this line*/}
 					<div className="row hero-area-slide">
 						<div className="col-lg-6 col-md-5">
 							<div className="hero-area-content">
@@ -152,27 +143,28 @@ function HeroArea() {
 								<p>She is a devil princess from the demon world. She grew up sheltered by her parents and doesn't really know how to be evil or any of the common actions,   She is unable to cry due to Keita's accidental first wish, despite needed for him to wish...</p>
 								<h3>Cast:</h3>
 								<div className="slide-cast">
-								  <div className="single-slide-cast">
+									<div className="single-slide-cast">
 										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast2} alt="about" />
+                                    <div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
+                                    <div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
+                                    <div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
+                                    <div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
+                                    <div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
+                                    <div className="single-slide-cast">
+										<img src={cast1} alt="about" />
 									</div>
+									
 									<div className="single-slide-cast text-center">
 										5+
 									</div>
@@ -211,27 +203,25 @@ function HeroArea() {
 									<p>She is a devil princess from the demon world. She grew up sheltered by her parents and doesn't really know how to be evil or any of the common actions,   She is unable to cry due to Keita's accidental first wish, despite needed for him to wish...</p>
 									<h3>Cast:</h3>
 									<div className="slide-cast">
-										  <div className="single-slide-cast">
-										<img src={cast1} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast2} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
+										<div className="single-slide-cast">
+											<img src={cast1} alt="about" />
+										</div>
+                                        	<div className="single-slide-cast">
+											<img src={cast1} alt="about" />
+										</div>
+                                        	<div className="single-slide-cast">
+											<img src={cast1} alt="about" />
+										</div>
+                                        	<div className="single-slide-cast">
+											<img src={cast1} alt="about" />
+										</div>
+                                        	<div className="single-slide-cast">
+											<img src={cast1} alt="about" />
+										</div>
+                                        	<div className="single-slide-cast">
+											<img src={cast1} alt="about" />
+										</div>
+									
 										<div className="single-slide-cast text-center">
 											5+
 										</div>
@@ -267,27 +257,25 @@ function HeroArea() {
 									<p>She is a devil princess from the demon world. She grew up sheltered by her parents and doesn't really know how to be evil or any of the common actions,   She is unable to cry due to Keita's accidental first wish, despite needed for him to wish...</p>
 									<h3>Cast:</h3>
 									<div className="slide-cast">
-										  <div className="single-slide-cast">
-										<img src={cast1} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast2} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
-									<div className="single-slide-cast">
-										<img src={cast3} alt="about" />
-									</div>
+										<div className="single-slide-cast">
+											<img src={cast} alt="about" />
+										</div>
+                                        <div className="single-slide-cast">
+											<img src={cast} alt="about" />
+										</div>
+                                        <div className="single-slide-cast">
+											<img src={cast} alt="about" />
+										</div>
+                                        <div className="single-slide-cast">
+											<img src={cast} alt="about" />
+										</div>
+                                        <div className="single-slide-cast">
+											<img src={cast} alt="about" />
+										</div>
+                                        <div className="single-slide-cast">
+											<img src={cast} alt="about" />
+										</div>
+										
 										<div className="single-slide-cast text-center">
 											5+
 										</div>
@@ -302,7 +290,7 @@ function HeroArea() {
 					</div>
 				</div>
 			</div>
-		</section>{/* hero area end */}
+		</section>
 
 
     </React.Fragment>
