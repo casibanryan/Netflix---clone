5f3e36447424a35756b2315aa683a1a30cfa7b89
casibanryan <restyochea@gmail.com>
2022-04-30 13:58:38 +0800

    updating

--- src/pages/sections/home/HeroArea.js
+++ src/pages/sections/home/HeroArea.js
@@ -3,13 +3,7 @@ import slide2 from '../../../assets/img/slide2.png'
 import slide3 from '../../../assets/img/slide3.png'
 import slide1 from '../../../assets/img/slide1.png'
 import cast1 from '../../../assets/img/cast/cast1.png';
-import '../../../css/home.css';
-import '../../../css/bootstrap.min.css';
-import '../../../css/slicknav.min.css';
-import '../../../css/icofont.css';
-import '../../../css/owl.carousel.css';
-import '../../../css/magnific-popup.css';
-import '../../../css/responsive.css'
+
 
 
 function HeroArea() {
@@ -55,121 +49,7 @@ function HeroArea() {
 							</div>
 						</div>
 					</div>
-					<div className="row hero-area-slide">
-						<div className="col-lg-6 col-md-5">
-							<div className="hero-area-content">
-								<img src={slide1} alt="about" />
-							</div>
-						</div>
-						<div className="col-lg-6 col-md-7">
-							<div className="hero-area-content pr-50">
-								<h2>Last Avatar</h2>
-								<div className="review">
-									<div className="author-review">
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-									</div>
-									<h4>180k voters</h4>
-								</div>
-								<p>She is a devil princess from the demon world. She grew up sheltered by her parents and doesn't really know how to be evil or any of the common actions,   She is unable to cry due to Keita's accidental first wish, despite needed for him to wish...</p>
-								<h3>Cast:</h3>
-								<div className="slide-cast">
-									<div className="single-slide-cast">
-										<img src={cast1} alt="about" />
-									</div>
-									
-									<div className="single-slide-cast text-center">
-										5+
-									</div>
-								</div>
-								<div className="slide-trailor">
-									<h3>Watch Trailer</h3>
-									<a className="theme-btn theme-btn2" href="#"><i className="icofont icofont-play"></i> Tickets</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="row hero-area-slide">
-						<div className="col-lg-6 col-md-5">
-							<div className="hero-area-content">
-								<img src={slide3} alt="about" />
-							</div>
-						</div>
-						<div className="col-lg-6 col-md-7">
-							<div className="hero-area-content pr-50">
-								<h2>The Deer God</h2>
-								<div className="review">
-									<div className="author-review">
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-									</div>
-									<h4>180k voters</h4>
-								</div>
-								<p>She is a devil princess from the demon world. She grew up sheltered by her parents and doesn't really know how to be evil or any of the common actions,   She is unable to cry due to Keita's accidental first wish, despite needed for him to wish...</p>
-								<h3>Cast:</h3>
-								<div className="slide-cast">
-									<div className="single-slide-cast">
-										<img src={cast1} alt="about" />
-									</div>
-									
-									<div className="single-slide-cast text-center">
-										5+
-									</div>
-								</div>
-								<div className="slide-trailor">
-									<h3>Watch Trailer</h3>
-									<a className="theme-btn theme-btn2" href="#"><i className="icofont icofont-play"></i> Tickets</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="hero-area-thumb">
-					<div className="thumb-prev">
-						<div className="row hero-area-slide">
-							<div className="col-lg-6">
-								<div className="hero-area-content">
-									<img src={slide3} alt="about" />
-								</div>
-							</div>
-							<div className="col-lg-6">
-								<div className="hero-area-content pr-50">
-									<h2>Last Avatar</h2>
-									<div className="review">
-										<div className="author-review">
-											<i className="icofont icofont-star"></i>
-											<i className="icofont icofont-star"></i>
-											<i className="icofont icofont-star"></i>
-											<i className="icofont icofont-star"></i>
-											<i className="icofont icofont-star"></i>
-										</div>
-										<h4>180k voters</h4>
-									</div>
-									<p>She is a devil princess from the demon world. She grew up sheltered by her parents and doesn't really know how to be evil or any of the common actions,   She is unable to cry due to Keita's accidental first wish, despite needed for him to wish...</p>
-									<h3>Cast:</h3>
-									<div className="slide-cast">
-										<div className="single-slide-cast">
-											<img src={cast1} alt="about" />
-										</div>
-									
-										<div className="single-slide-cast text-center">
-											5+
-										</div>
-									</div>
-									<div className="slide-trailor">
-										<h3>Watch Trailer</h3>
-										<a className="theme-btn theme-btn2" href="#"><i className="icofont icofont-play"></i> Tickets</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+				
 					<div className="thumb-next">
 						<div className="row hero-area-slide">
 							<div className="col-lg-6">
