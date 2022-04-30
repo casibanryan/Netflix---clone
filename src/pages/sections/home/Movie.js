1deb6b39a7ab0fd2a5f989759f29e3b25dc66435
casibanryan <restyochea@gmail.com>
2022-04-30 14:27:13 +0800

    dsjdjskd

--- src/pages/sections/home/Movie.js
+++ src/pages/sections/home/Movie.js
@@ -19,18 +19,18 @@ import sidebar4 from '../../../assets/img/sidebar/sidebar4.png';
 function Movie() {
   return (
     <>
-		<section class="portfolio-area pt-60">
-			<div class="container">
-				<div class="row flexbox-center">
-					<div class="col-lg-6 text-center text-lg-left">
-					    <div class="section-title">
-							<h1><i class="icofont icofont-movie"></i> Spotlight This Month</h1>
+		<section className="portfolio-area pt-60">
+			<div className="container">
+				<div className="row flexbox-center">
+					<div className="col-lg-6 text-center text-lg-left">
+					    <div className="section-title">
+							<h1><i className="icofont icofont-movie"></i> Spotlight This Month</h1>
 						</div>
 					</div>
-					<div class="col-lg-6 text-center text-lg-right">
-					    <div class="portfolio-menu">
+					<div className="col-lg-6 text-center text-lg-right">
+					    <div className="portfolio-menu">
 							<ul>
-								<li data-filter="*" class="active">Latest</li>
+								<li data-filter="*" className="active">Latest</li>
 								<li data-filter=".soon">Coming Soon</li>
 								<li data-filter=".top">Trending Now</li>
 								<li data-filter=".released">Recently Released</li>
@@ -39,141 +39,141 @@ function Movie() {
 					</div>
 				</div>
 				<hr />
-				<div class="row">
-					<div class="col-lg-9">
-						<div class="row portfolio-item">
-							<div class="col-md-4 col-sm-6 soon released">
-								<div class="single-portfolio">
-									<div class="single-portfolio-img">
+				<div className="row">
+					<div className="col-lg-9">
+						<div className="row portfolio-item">
+							<div className="col-md-4 col-sm-6 soon released">
+								<div className="single-portfolio">
+									<div className="single-portfolio-img">
 										<img src={portfolio1} alt="portfolio" />
-										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" class="popup-youtube">
-											<i class="icofont icofont-ui-play"></i>
+										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" className="popup-youtube">
+											<i className="icofont icofont-ui-play"></i>
 										</a>
 									</div>
-									<div class="portfolio-content">
+									<div className="portfolio-content">
 										<h2>Boyz II Men</h2>
-										<div class="review">
-											<div class="author-review">
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
+										<div className="review">
+											<div className="author-review">
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
 											</div>
 											<h4>180k voters</h4>
 										</div>
 									</div>
 								</div>
 							</div>
-							<div class="col-md-4 col-sm-6 top">
-								<div class="single-portfolio">
-									<div class="single-portfolio-img">
+							<div className="col-md-4 col-sm-6 top">
+								<div className="single-portfolio">
+									<div className="single-portfolio-img">
 										<img src={portfolio2} alt="portfolio" />
-										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" class="popup-youtube">
-											<i class="icofont icofont-ui-play"></i>
+										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" className="popup-youtube">
+											<i className="icofont icofont-ui-play"></i>
 										</a>
 									</div>
-									<div class="portfolio-content">
+									<div className="portfolio-content">
 										<h2>Tale of Revemge</h2>
-										<div class="review">
-											<div class="author-review">
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
+										<div className="review">
+											<div className="author-review">
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
 											</div>
 											<h4>180k voters</h4>
 										</div>
 									</div>
 								</div>
 							</div>
-							<div class="col-md-4 col-sm-6 soon">
-								<div class="single-portfolio">
-									<div class="single-portfolio-img">
+							<div className="col-md-4 col-sm-6 soon">
+								<div className="single-portfolio">
+									<div className="single-portfolio-img">
 										<img src={portfolio3} alt="portfolio" />
-										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" class="popup-youtube">
-											<i class="icofont icofont-ui-play"></i>
+										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" className="popup-youtube">
+											<i className="icofont icofont-ui-play"></i>
 										</a>
 									</div>
-									<div class="portfolio-content">
+									<div className="portfolio-content">
 										<h2>The Lost City of Z</h2>
-										<div class="review">
-											<div class="author-review">
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
+										<div className="review">
+											<div className="author-review">
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
 											</div>
 											<h4>180k voters</h4>
 										</div>
 									</div>
 								</div>
 							</div>
-							<div class="col-md-4 col-sm-6 top released">
-								<div class="single-portfolio">
-									<div class="single-portfolio-img">
+							<div className="col-md-4 col-sm-6 top released">
+								<div className="single-portfolio">
+									<div className="single-portfolio-img">
 										<img src={portfolio4} alt="portfolio" />
-										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" class="popup-youtube">
-											<i class="icofont icofont-ui-play"></i>
+										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" className="popup-youtube">
+											<i className="icofont icofont-ui-play"></i>
 										</a>
 									</div>
-									<div class="portfolio-content">
+									<div className="portfolio-content">
 										<h2>Beast Beauty</h2>
-										<div class="review">
-											<div class="author-review">
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
+										<div className="review">
+											<div className="author-review">
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
 											</div>
 											<h4>180k voters</h4>
 										</div>
 									</div>
 								</div>
 							</div>
-							<div class="col-md-4 col-sm-6 released">
-								<div class="single-portfolio">
-									<div class="single-portfolio-img">
+							<div className="col-md-4 col-sm-6 released">
+								<div className="single-portfolio">
+									<div className="single-portfolio-img">
 										<img src={portfolio5} alt="portfolio" />
-										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" class="popup-youtube">
-											<i class="icofont icofont-ui-play"></i>
+										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" className="popup-youtube">
+											<i className="icofont icofont-ui-play"></i>
 										</a>
 									</div>
-									<div class="portfolio-content">
+									<div className="portfolio-content">
 										<h2>In The Fade</h2>
-										<div class="review">
-											<div class="author-review">
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
+										<div className="review">
+											<div className="author-review">
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
 											</div>
 											<h4>180k voters</h4>
 										</div>
 									</div>
 								</div>
 							</div>
-							<div class="col-md-4 col-sm-6 soon top">
-								<div class="single-portfolio">
-									<div class="single-portfolio-img">
+							<div className="col-md-4 col-sm-6 soon top">
+								<div className="single-portfolio">
+									<div className="single-portfolio-img">
 										<img src={portfolio6} alt="portfolio" />
-										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" class="popup-youtube">
-											<i class="icofont icofont-ui-play"></i>
+										<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" className="popup-youtube">
+											<i className="icofont icofont-ui-play"></i>
 										</a>
 									</div>
-									<div class="portfolio-content">
+									<div className="portfolio-content">
 										<h2>Last Hero</h2>
-										<div class="review">
-											<div class="author-review">
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
-												<i class="icofont icofont-star"></i>
+										<div className="review">
+											<div className="author-review">
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
+												<i className="icofont icofont-star"></i>
 											</div>
 											<h4>180k voters</h4>
 										</div>
@@ -182,8 +182,8 @@ function Movie() {
 							</div>
 						</div>
 					</div>
-					<div class="col-lg-3 text-center text-lg-left">
-					    <div class="portfolio-sidebar">
+					<div className="col-lg-3 text-center text-lg-left">
+					    <div className="portfolio-sidebar">
 							<img src={sidebar1} alt="sidebar" />
                             <img src={sidebar2} alt="sidebar" />
                             <img src={sidebar3} alt="sidebar" />
@@ -194,53 +194,53 @@ function Movie() {
 				</div>
 			</div>
 		</section>
-		<section class="video ptb-90">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-					    <div class="section-title pb-20">
-							<h1><i class="icofont icofont-film"></i> Continue Watching</h1>
+		<section className="video ptb-90">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-12">
+					    <div className="section-title pb-20">
+							<h1><i className="icofont icofont-film"></i> Continue Watching</h1>
 						</div>
 					</div>
 				</div>
 				<hr />
-				<div class="row">
-                    <div class="col-md-9">
-						<div class="video-area">
+				<div className="row">
+                    <div className="col-md-9">
+						<div className="video-area">
 							<img src={video1} alt="video" />
-							<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" class="popup-youtube">
-								<i class="icofont icofont-ui-play"></i>
+							<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" className="popup-youtube">
+								<i className="icofont icofont-ui-play"></i>
 							</a>
-							<div class="video-text">
+							<div className="video-text">
 								<h2>Angle of Death</h2>
-								<div class="review">
-									<div class="author-review">
-										<i class="icofont icofont-star"></i>
-										<i class="icofont icofont-star"></i>
-										<i class="icofont icofont-star"></i>
-										<i class="icofont icofont-star"></i>
-										<i class="icofont icofont-star"></i>
+								<div className="review">
+									<div className="author-review">
+										<i className="icofont icofont-star"></i>
+										<i className="icofont icofont-star"></i>
+										<i className="icofont icofont-star"></i>
+										<i className="icofont icofont-star"></i>
+										<i className="icofont icofont-star"></i>
 									</div>
 									<h4>180k voters</h4>
 								</div>
 							</div>
 						</div>
                     </div>
-                    <div class="col-md-3">
-						<div class="row">
-							<div class="col-md-12 col-sm-6">
-								<div class="video-area">
+                    <div className="col-md-3">
+						<div className="row">
+							<div className="col-md-12 col-sm-6">
+								<div className="video-area">
 										<img src={video2} alt="video" />
-									<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" class="popup-youtube">
-										<i class="icofont icofont-ui-play"></i>
+									<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" className="popup-youtube">
+										<i className="icofont icofont-ui-play"></i>
 									</a>
 								</div>
 							</div>
-							<div class="col-md-12 col-sm-6">
-								<div class="video-area">
+							<div className="col-md-12 col-sm-6">
+								<div className="video-area">
 									<img src={video3} alt="video" />
-									<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" class="popup-youtube">
-										<i class="icofont icofont-ui-play"></i>
+									<a href="https://www.youtube.com/watch?v=RZXnugbhw_4" className="popup-youtube">
+										<i className="icofont icofont-ui-play"></i>
 									</a>
 								</div>
 							</div>
