1743bb8cd53b12d48a65ba5cd02f43a1c0b9cf3f
casibanryan <restyochea@gmail.com>
2022-05-01 17:22:03 +0800

    updating

--- src/Home.js
+++ src/Home.js
@@ -44,36 +44,35 @@ function Home({ title, fetchUrl, isLargeRow }) {
                     <div className="modal-dialog modal-fullscreen">
                         <div className="modal-content bg__dark">
                         <div className="modal-header" style={{ borderBottom:"none"}}>
-                            <h1 className="modal-title" id="staticBackdropLabel">  {movie?.title || movie?.name || movie?.original_name} </h1>
                             <button type="button" className="btn-close btn-lg bg-danger" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                                
-		<section className="hero-area" id="home">
-			<div className="container">
-				<div className="hero-area-slider">
-					<div className="row hero-area-slide">
-						<div className="col-lg-6 col-md-5">
-							<div className="hero-area-content">
-								<img src={`${baseUrl}${movie?.poster_path || movie?.backdrop_path}`} alt={movie.name} />
-							</div>
-						</div>
-						<div className="col-lg-6 col-md-7">
-							<div className="hero-area-content pr-50">
-								<h2>The Devil Princess</h2>
-								<div className="review">
-									<div className="author-review">
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-										<i className="icofont icofont-star"></i>
-									</div>
-									<h4>180k voters</h4>
-								</div>
-								<p>She is a devil princess from the demon world. She grew up sheltered by her parents and doesn't really know how to be evil or any of the common actions,   She is unable to cry due to Keita's accidental first wish, despite needed for him to wish...</p>
-								<h3>Cast:</h3>
-								<div className="slide-cast">
+                                            
+                    <section className="hero-area" id="home">
+                        <div className="container">
+                            <div className="hero-area-slider">
+                                <div className="row hero-area-slide">
+                                    <div className="col-lg-6 col-md-5">
+                                        <div className="hero-area-content">
+                                            <img src={`${baseUrl}${movie.poster_path}`} alt={movie.name} />
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-6 col-md-7">
+                                        <div className="hero-area-content pr-50">
+                                            <h1> {movie?.title || movie?.name || movie?.original_name}</h1>
+                                            <div className="review">
+                                                <div className="author-review">
+                                                    <i className="icofont icofont-star"></i>
+                                                    <i className="icofont icofont-star"></i>
+                                                    <i className="icofont icofont-star"></i>
+                                                    <i className="icofont icofont-star"></i>
+                                                    <i className="icofont icofont-star"></i>
+                                                </div>
+                                                <h4>180k voters</h4>
+                                            </div>
+                                            <p>She is a devil princess from the demon world. She grew up sheltered by her parents and doesn't really know how to be evil or any of the common actions,   She is unable to cry due to Keita's accidental first wish, despite needed for him to wish...</p>
+                                            <h3>Cast:</h3>
+                                            <div className="slide-cast">
 									{/* <div className="single-slide-cast">
 										<img src="assets/img/cast/cast1.png" alt="about" />
 									</div>
