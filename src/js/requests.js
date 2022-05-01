c1e5dfbd95f4975ad27fea72fa85100b3057f1ca
casibanryan <restyochea@gmail.com>
2022-05-01 09:26:50 +0800

    debugging

--- src/js/requests.js
+++ src/js/requests.js
@@ -6,7 +6,7 @@ const API_KEY = "14d57525a7a6772b9699d35339fb5878";
 // https://api.themoviedb.org/3//trending/all/week?api_key=14d57525a7a6772b9699d35339fb5878
 const requests = {
 
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTrending: `/trending/all/month?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
