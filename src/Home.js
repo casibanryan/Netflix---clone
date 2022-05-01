e1c34c7dad20dc769cd061ac6715168e3430e700
casibanryan <restyochea@gmail.com>
2022-05-01 16:23:49 +0800

    updating

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from './js/axios';
 import './css/Home.css';
-
+import {Helmet} from 'react-helmet';
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
@@ -33,7 +33,8 @@ function Home({ title, fetchUrl, isLargeRow }) {
                 <div className="row__posters">
                      {/* looping the data  */}
                     {movies.map((movie) => (
-                        <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><img className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
+                        <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                        <img className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
                         key={movie.id}
                         src={`${baseUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
                         alt={ movie.name }/>
@@ -61,7 +62,10 @@ function Home({ title, fetchUrl, isLargeRow }) {
 
             </div>
         </div>
+                        <Helmet>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+                        </Helmet>
     </React.Fragment>
   )
 }
