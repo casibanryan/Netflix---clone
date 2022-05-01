dc8e9d68f9669f8b3d8c77ac22ec7e323c9e0106
casibanryan <restyochea@gmail.com>
2022-05-01 20:14:20 +0800

    updating

--- src/js/starRating.js
+++ src/js/starRating.js
@@ -4,23 +4,13 @@ import React from 'react'
 
 function starRating(props) {
     
-    if(props > 1000) {
-    return (
-        <>
-          <AiTwotoneStar />
-          <AiTwotoneStar />
-            <AiTwotoneStar />
-              <AiTwotoneStar />
-                <AiTwotoneStar />
-        </>
-  )
+    const div = document.createElement('div');
+    for(let index = 0; index < (props > 1000) ? 5 : (props > 500 && props <1000) ? 4 : 3; index++) {
+        div.append(<AiTwotoneStar />)
     }
 
-    else {
-         <>
-          <AiTwotoneStar />
-        </>
-    }
+    return div;
+
 }
 
 export default starRating;
