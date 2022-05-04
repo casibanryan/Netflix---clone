8f180f85e478853cf87ccfcc4cb4e8447adcac9e
casibanryan <restyochea@gmail.com>
2022-05-04 10:23:30 +0800

    updating

--- src/sections/Banner.js
+++ src/sections/Banner.js
@@ -62,24 +62,18 @@ function Banner() {
         </div>
         <div className="banner__fadeBottom"></div>
 
-<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div className="modal-dialog modal-fullscreen">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="staticBackdropLabel">Modal title</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-       
-        <YouTube videoId="2g811Eo7K8U" opts={opts} />;
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Understood</button>
-      </div>
-    </div>
-  </div>
-</div>
+        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div className="modal-dialog modal-fullscreen">
+            <div className="modal-content">
+              <div className="modal-body">
+                <YouTube videoId="2g811Eo7K8U" opts={opts} />;
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-primary">close</button>
+              </div>
+            </div>
+          </div>
+        </div>
 
     </header>
   )
