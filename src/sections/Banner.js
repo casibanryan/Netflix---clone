3f9491f720c214109d694be36fb460ed331c34eb
casibanryan <restyochea@gmail.com>
2022-05-04 11:10:59 +0800

    updating

--- src/sections/Banner.js
+++ src/sections/Banner.js
@@ -3,34 +3,49 @@ import axios from '../js/axios';
 import requests from '../js/requests';
 import '../css/Banner.css';
 import YouTube from 'react-youtube';
-
+import movieTrailer from 'movie-trailer';
 
 
 function Banner() {
     const [movie, setMovie] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     useEffect(() => {
       async function fetchData() {
-
         const request = await axios.get(requests.fetchTrending);
         // getting 1 (random) movie only in trending section
         setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]);
         return request;
-           
       }
       fetchData();
     }, [])
+
+
     // minimize the text in small screen (responsive)
     function truncate(str, n) {
             return str?.length > n ? str.substr(0, n-1) + "..." : str;
     }
 
      const opts = {
-      height: '800px',
+      height: '800',
       width: '100%',
       playerVars: {
         // https://developers.google.com/youtube/player_parameters
         autoplay: 1,
+      }
+    }
+
+    const handleClick = (movie) => {
+      if(trailerUrl) {
+        setTrailerUrl('');
+      } else {
+        movieTrailer(movie?.name || "")
+          .then(url => {
+            //https://www.youtube.com/watch?v=XtMThy8QKqU
+            //get everything after the  ?
+            const urlParams = new URLSearchParams(URL(url).search);
+            setTrailerUlr(urlParams.get("v"));
+          }).catch((error) => console.log(error));
       }
     }
   return (
@@ -51,7 +66,9 @@ function Banner() {
                 <div className="banner__buttons">
                     <button className="banner__button" 
                             type="button" data-bs-toggle="modal" 
-                            data-bs-target="#staticBackdrop">
+                            data-bs-target="#staticBackdrop"
+                            onClick={handleClick(movie)}
+                            >
                               Play
                     </button>
                       <button className="banner__button">My List</button>
@@ -62,18 +79,18 @@ function Banner() {
         </div>
         <div className="banner__fadeBottom"></div>
 
-<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div className="modal-dialog modal-fullscreen">
-    <div className="modal-content bg__dark">
-      <div className="modal-header" style={{ borderBottom:"none"}}>
-        <button type="button" className="btn-close btn-lg bg-danger" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-        <YouTube videoId="2g811Eo7K8U" opts={opts} />
-      </div>
-    </div>
-  </div>
-</div>
+        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div className="modal-dialog modal-fullscreen">
+            <div className="modal-content bg__dark">
+              <div className="modal-header" style={{ borderBottom:"none"}}>
+                <button type="button" className="btn-close btn-lg bg-danger" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+              </div>
+            </div>
+          </div>
+        </div>
 
     </header>
   )
