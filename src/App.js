522815bc178adce3841fab828daafdcfac8d17ec
casibanryan <restyochea@gmail.com>
2022-05-05 14:20:10 +0800

    updating

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ function App() {
         setData(watchList);
     })
 
+    console.log(data);
+
   return (
 
     <React.Fragment>
@@ -25,7 +27,7 @@ function App() {
       {/* banner */}
           <Banner />
             <Home title="New Release" fetchUrl={requests.fetchNetflixOriginals} fetchPeople={requests.fetchPeople} isLargeRow={true} />
-            <ContinueWatching title="Continue Watching" watchList={watchList}/>
+            <ContinueWatching title="Continue Watching" watchList={data}/>
             <Home title="Trending Now" fetchUrl={requests.fetchTrending} fetchPeople={requests.fetchPeople} /> 
             <Home title="Top Rated" fetchUrl={requests.fetchTopRated} fetchPeople={requests.fetchPeople} /> 
             <Home title="Action Movies" fetchUrl={requests.fetchActionMovies} fetchPeople={requests.fetchPeople} /> 
