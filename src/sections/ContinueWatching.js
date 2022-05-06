600953f77aa4712cb334a13d15cac3552d1cd3a1
casibanryan <restyochea@gmail.com>
2022-05-06 11:00:57 +0800

    ...

--- src/sections/ContinueWatching.js
+++ src/sections/ContinueWatching.js
@@ -1,42 +1,17 @@
 import React from 'react';
 import YouTube from 'react-youtube';
-import screenfull from "screenfull";  
-import DeviceDetector from "device-detector-js"; 
-import { makeStyles } from "@material-ui/core/styles"; 
+
 
 function ContinueWatching({title, watchList}) {
 
-/* Checking if the user is using a mobile browser. */
-  const dd = new DeviceDetector();  
-  const mobile = dd.usesMobileBrowser();
-
- /**
-  * The function takes an event as an argument and then uses the event to get the iframe element. If
-  * the screenfull library is enabled, the function requests the iframe to go fullscreen.
-  */
-  const fullScreen = event => {  
-      var iframe = event.target.getIframe();  
-      if (screenfull.isEnabled) {  
-        screenfull.request(iframe);  
-   }  
-  };  
 
     /* The options for the youtube video. */
      const opts = {
           height: '100',
           width: '200',
           controls: "1",
-           origin: 'http://localhost:3000/home' 
+          origin: 'http://localhost:3000/home' 
     }
-
-    const useStyles = makeStyles({  
-        videoDiv: {  
-          width: "100%",  
-          height: "100%"  
-        }  
-    });  
-
-     const classes = useStyles();  
 
   return (
      <div className={`home__row ${watchList.length == 0 ? "d-none" : "d-block" }`} id="my_list">
@@ -49,8 +24,7 @@ function ContinueWatching({title, watchList}) {
                            <YouTube  key={index} 
                                      videoId={trailerUrl.movie} 
                                      opts={opts} 
-                                      className={`row__poster ${classes.videoDiv}`}
-                                      onPlay={() => (mobile ? fullScreen : {})}  
+                                      className="row__poster"
 
                                      />
                      ))}
