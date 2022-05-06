69bfa5b7cb57135ca9fd83ae038bc710d3cb5d86
casibanryan <restyochea@gmail.com>
2022-05-06 11:22:46 +0800

    ...

--- src/sections/ContinueWatching.js
+++ src/sections/ContinueWatching.js
@@ -10,9 +10,15 @@ function ContinueWatching({title, watchList}) {
           height: '100',
           width: '200',
           controls: "1",
-          origin: 'http://192.168.1.2:3000' 
     }
 
+    // pause the video if clicked
+    const handleClose = () => {
+        const closeBtn = document.getElementById('close');
+        if(!closeBtn.clicked) setTrailerUrl('');
+    };
+
+    var trailer;
   return (
      <div className={`home__row ${watchList.length == 0 ? "d-none" : "d-block" }`} id="my_list">
               {/* title */}
@@ -21,13 +27,33 @@ function ContinueWatching({title, watchList}) {
                 <div className="row__posters">
                      {/* looping the data  */}
                     {watchList.map((trailerUrl, index) => (
+                        <>
                            <YouTube  key={index} 
                                      videoId={trailerUrl.movie} 
+                                     trailer = {trailerUrl.movie}
                                      opts={opts} 
                                       className="row__poster"
-
+                                      data-bs-toggle="modal" data-bs-target={`#continue_watchingModal${index}`}
                                      />
+                            <div className="modal fade" id={`continue_watchingModal${index}`} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                              <div className="modal-dialog modal-fullscreen">
+                                <div className="modal-content bg__dark">
+                                  <div className="modal-header" style={{ borderBottom:"none"}}>
+                                    <button type="button" className="btn-close btn-lg bg-danger" 
+                                            data-bs-dismiss="modal"
+                                            id="continue_close" aria-label="Close"
+                                            onClick={() => handleClose()}           
+                                            ></button>
+                                  </div>
+                                  <div className="modal-body">
+                                  <YouTube videoId={trailer} opts={opts} />
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            </>
                      ))}
+                      
             </div>
         </div>
   )
