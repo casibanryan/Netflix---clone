b13ed9a3e90f6ed01f080e53c5c2448e9b83e654
casibanryan <restyochea@gmail.com>
2022-05-06 12:08:08 +0800

    ...

--- src/sections/ContinueWatching.js
+++ src/sections/ContinueWatching.js
@@ -1,9 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import YouTube from 'react-youtube';
 
 
 function ContinueWatching({title, watchList}) {
-
 
     /* The options for the youtube video. */
      const opts = {
@@ -11,13 +10,6 @@ function ContinueWatching({title, watchList}) {
           width: '200',
           controls: "1",
     }
-
-    const [trailer, setTrailer] = useState[""];
-
-      const handleTrailer = (movie) => {
-          // pause the movie if the user clicks another video
-          movie ? setTrailer('') : setTrailer(movie)
-      }
                 
   return (
      <div className={`home__row ${watchList.length == 0 ? "d-none" : "d-block" }`} id="my_list">
@@ -27,16 +19,12 @@ function ContinueWatching({title, watchList}) {
                 <div className="row__posters">
                      {/* looping the data  */}
                     {watchList.map((trailerUrl, i) => (
-                        <React.Fragment>
                            <YouTube  key={i}
                                      videoId={trailerUrl.movie} 
                                      opts={opts} 
                                      className="row__poster"
-                                     onCLick={() => handleTrailer(watchList)}
-                                     />
-                          </React.Fragment>
+                              />
                      ))}
-                      
             </div>
         </div>
   )
